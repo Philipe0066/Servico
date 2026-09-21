d67e6f26bda16661fa2fdbe8497c3a1ee8f13020
Philipe0066 <p667583@gmail.com>
2026-09-20 23:47:29 -0300

    Update service worker cache name and event listeners

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = 'instalacoes-v2.1';
+const CACHE_NAME = 'instalacoes-v2.2';
 
-self.addEventListener('install', (e) => {
+self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  return self.clients.claim();
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
